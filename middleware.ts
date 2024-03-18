bad7ff37b3ef2fa15d2d17430c2e246cacdc8dfa
Christof Karisch <ckarisch@outlook.com>
2024-03-19 00:17:35 +0100

    allow secure cookies

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,13 @@ import { withAuth } from "next-auth/middleware"
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('next-auth.session-token')?.value
+  const currentUserSecure = request.cookies.get('__Secure-next-auth.session-token')?.value
 
   // if (currentUser && !request.nextUrl.pathname.startsWith('/app')) {
   //   return Response.redirect(new URL('/app', request.url))
   // }
 
-  if (!currentUser && request.nextUrl.pathname.startsWith('/app') && !request.nextUrl.pathname.startsWith('/api/auth/signin')) {
+  if (!currentUser && !currentUserSecure && request.nextUrl.pathname.startsWith('/app') && !request.nextUrl.pathname.startsWith('/api/auth/signin')) {
     return Response.redirect(new URL('/api/auth/signin', request.url))
   }
 }
