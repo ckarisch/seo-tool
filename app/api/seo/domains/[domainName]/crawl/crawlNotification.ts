4d075d50189d583cb4e44641acdd3e9171d813a7
Christof Karisch <ckarisch@outlook.com>
2024-04-02 14:08:50 +0200

    use mailgun

--- app/api/seo/domains/[domainName]/crawl/crawlNotification.ts
+++ app/api/seo/domains/[domainName]/crawl/crawlNotification.ts
@@ -31,32 +31,51 @@ export const crawlNotification = (userWithNotificationContacts: any, type: crawl
 
 
 export const sendNotification = (toEmail: string, toName: string, title: string, message: string, domain: string, urls: string[]) => {
-    let apiInstance = new brevo.TransactionalEmailsApi();
 
-    let apiKey = apiInstance.authentications['apiKey'];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
-    let sendSmtpEmail = new brevo.SendSmtpEmail();
+    var api_key = process.env.MAILGUN_SENDING_API_KEY;
+    var domain = process.env.MAILGUN_SENDING_DOMAIN!;
+    var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
-    sendSmtpEmail.subject = 'Neue Benachrichtigung';
-    sendSmtpEmail.htmlContent = '<html><body><h1>{{params.heading}}</h1><br/><div>{{params.message}}</div><div>Betroffene URLs: <div>' + urls.join('<br/>') + '</div></div><br/><div><a href="{{params.appDomain}}/app/domains/{{params.domain}}">Details anzeigen</a></div></body></html>';
-    sendSmtpEmail.sender = { 'name': 'SEO Notification', 'email': 'notification@formundzeichen.at' };
-    sendSmtpEmail.to = [
-        { 'email': toEmail, 'name': toName }
-    ];
-    sendSmtpEmail.replyTo = { 'email': 'notification@formundzeichen.at', 'name': 'SEO Notification' };
-    // sendSmtpEmail.headers = { 'Some-Custom-Name': 'unique-id-1234' };
-    sendSmtpEmail.params = {
-        'heading': title,
-        'message': message,
-        'subject': 'Neue Benachrichtigung',
-        'appDomain': process.env.NEXT_PUBLIC_API_DOMAIN,
-        'domain': domain
+    const messageHtml = '<html><body><h1>' + title + '</h1><br/><div>' + message + '</div><div>Betroffene URLs: <div>' + urls.join('<br/>') + '</div></div><br/><div><a href="' + process.env.NEXT_PUBLIC_API_DOMAIN + '/app/domains/' + domain + '">Details anzeigen</a></div></body></html>';
+
+    var data = {
+        from: 'SEO Notification <notification@formundzeichen.at>',
+        to: toName + '<' + toEmail + '>',
+        subject: 'Neue Benachrichtigung',
+        text: messageHtml,
+        html: messageHtml
     };
 
-    apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data: any) {
-        console.log('notification sent')
-        // console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-    }, function (error: any) {
-        console.error(error);
+    mailgun.messages().send(data, function (error: any, body: any) {
+        console.log(body);
     });
+
+    // let apiInstance = new brevo.TransactionalEmailsApi();
+
+    // let apiKey = apiInstance.authentications['apiKey'];
+    // apiKey.apiKey = process.env.BREVO_API_KEY;
+    // let sendSmtpEmail = new brevo.SendSmtpEmail();
+
+    // sendSmtpEmail.subject = 'Neue Benachrichtigung';
+    // sendSmtpEmail.htmlContent = '<html><body><h1>{{params.heading}}</h1><br/><div>{{params.message}}</div><div>Betroffene URLs: <div>' + urls.join('<br/>') + '</div></div><br/><div><a href="{{params.appDomain}}/app/domains/{{params.domain}}">Details anzeigen</a></div></body></html>';
+    // sendSmtpEmail.sender = { 'name': 'SEO Notification', 'email': 'notification@formundzeichen.at' };
+    // sendSmtpEmail.to = [
+    //     { 'email': toEmail, 'name': toName }
+    // ];
+    // sendSmtpEmail.replyTo = { 'email': 'notification@formundzeichen.at', 'name': 'SEO Notification' };
+    // // sendSmtpEmail.headers = { 'Some-Custom-Name': 'unique-id-1234' };
+    // sendSmtpEmail.params = {
+    //     'heading': title,
+    //     'message': message,
+    //     'subject': 'Neue Benachrichtigung',
+    //     'appDomain': process.env.NEXT_PUBLIC_API_DOMAIN,
+    //     'domain': domain
+    // };
+
+    // apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data: any) {
+    //     console.log('notification sent')
+    //     // console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+    // }, function (error: any) {
+    //     console.error(error);
+    // });
 }
