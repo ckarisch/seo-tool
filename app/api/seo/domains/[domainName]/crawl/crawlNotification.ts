ded079302a22cc751c68200339ed0dfeb33a013e
Christof Karisch <ckarisch@outlook.com>
2024-04-05 00:22:57 +0200

    use json transport

--- app/api/seo/domains/[domainName]/crawl/crawlNotification.ts
+++ app/api/seo/domains/[domainName]/crawl/crawlNotification.ts
@@ -1,9 +1,48 @@
+import * as nodemailer from "nodemailer";
+import { MailOptions } from "nodemailer/lib/json-transport";
 
-const brevo = require('@getbrevo/brevo');
-import { PrismaClient } from "@prisma/client";
-const nodemailer = require("nodemailer");
+export class Emailer {
+    private readonly transporter: nodemailer.Transporter;
 
-const prisma = new PrismaClient();
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST!,
+            port: parseInt(process.env.MAIL_PORT!),
+            auth: {
+                user: process.env.MAIL_USER!, // generated ethereal user
+                pass: process.env.MAIL_PASSWORD! // generated ethereal password
+            }
+
+        });
+    }
+
+    public sendEmail(mailOptions: MailOptions) {
+        return this.transporter.sendMail(mailOptions);
+    }
+
+    public sendNotification(toEmail: string, messageHtml: string) {
+        this.sendEmail(sendMessageTemplate(toEmail, messageHtml));
+    }
+
+}
+
+export const emailer = new Emailer();
+
+
+export const sendMessageTemplate = (
+    toEmail: string,
+    messageHtml: string
+) => {
+    return {
+        from: "SEO Notification <notification@formundzeichen.at>",
+        to: [toEmail], // list of receivers
+        subject: "Neue Benachrichtigung",
+        text: messageHtml,
+        html: messageHtml,
+        replyTo: 'notification@formundzeichen.at'
+    } as MailOptions;
+};
+
 
 export enum crawlNotificationType {
     Error503,
@@ -37,34 +76,7 @@ export const sendNotification = async (toEmail: string, toName: string, title: s
     const messageHtml = '<html><body><h1>' + title + '</h1><br/><div>' + message + '</div><div>Betroffene URLs: <div>' + urls.join('<br/>') + '</div></div><br/><div><a href="' + process.env.NEXT_PUBLIC_API_DOMAIN + '/app/domains/' + domain + '">Details anzeigen</a></div></body></html>';
 
     try {
-
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.MAIL_USER, // generated ethereal user
-                pass: process.env.MAIL_PASSWORD // generated ethereal password
-            }
-        });
-
-        console.log('mail info');
-        const mailInfo = {
-            from: "SEO Notification <notification@formundzeichen.at>",
-            to: [toEmail], // list of receivers
-            subject: "Neue Benachrichtigung", // Subject line
-            text: messageHtml,
-            html: messageHtml,
-            replyTo: 'notification@formundzeichen.at'
-        };
-        console.log(mailInfo);
-
-        console.log("sending mail");
-        // send mail with defined transport object
-        let info = await transporter.sendMail(mailInfo);
-
-        console.log(info);
-
+        emailer.sendNotification(toEmail, messageHtml);
         console.log('message sent');
 
     }
@@ -73,32 +85,4 @@ export const sendNotification = async (toEmail: string, toName: string, title: s
         console.log(e);
     }
 
-    // let apiInstance = new brevo.TransactionalEmailsApi();
-
-    // let apiKey = apiInstance.authentications['apiKey'];
-    // apiKey.apiKey = process.env.BREVO_API_KEY;
-    // let sendSmtpEmail = new brevo.SendSmtpEmail();
-
-    // sendSmtpEmail.subject = 'Neue Benachrichtigung';
-    // sendSmtpEmail.htmlContent = '<html><body><h1>{{params.heading}}</h1><br/><div>{{params.message}}</div><div>Betroffene URLs: <div>' + urls.join('<br/>') + '</div></div><br/><div><a href="{{params.appDomain}}/app/domains/{{params.domain}}">Details anzeigen</a></div></body></html>';
-    // sendSmtpEmail.sender = { 'name': 'SEO Notification', 'email': 'notification@formundzeichen.at' };
-    // sendSmtpEmail.to = [
-    //     { 'email': toEmail, 'name': toName }
-    // ];
-    // sendSmtpEmail.replyTo = { 'email': 'notification@formundzeichen.at', 'name': 'SEO Notification' };
-    // // sendSmtpEmail.headers = { 'Some-Custom-Name': 'unique-id-1234' };
-    // sendSmtpEmail.params = {
-    //     'heading': title,
-    //     'message': message,
-    //     'subject': 'Neue Benachrichtigung',
-    //     'appDomain': process.env.NEXT_PUBLIC_API_DOMAIN,
-    //     'domain': domain
-    // };
-
-    // apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data: any) {
-    //     console.log('notification sent')
-    //     // console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-    // }, function (error: any) {
-    //     console.error(error);
-    // });
 }
