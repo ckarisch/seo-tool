ab5338e9255e43dcf32157a36217652bd5834982
Christof Karisch <ckarisch@outlook.com>
2024-10-26 16:24:53 +0200

    domain ordering

--- app/api/seo/domains/route.ts
+++ app/api/seo/domains/route.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from '@prisma/client'
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession } from 'next-auth';
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 
-import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/authOptions';
-import { VerificationCodeGenerator } from '@/util/api/domainVerificationKey';
-
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
+import { VerificationCodeGenerator } from "@/util/api/domainVerificationKey";
 
 const prisma = new PrismaClient();
 
@@ -20,57 +19,88 @@ export async function GET(req: NextRequest, res: NextResponse) {
     authOptions
   );
 
-  console.log('session', session)
+  console.log("session", session);
 
   if (!session || !session!.user) {
-    console.log('no session')
-    return Response.json({ error: 'Not authenticated', domains: [] }, { status: 401 })
+    console.log("no session");
+    return Response.json(
+      { error: "Not authenticated", domains: [] },
+      { status: 401 }
+    );
   }
-  const user = await prisma.user.findUnique({ where: { email: session!.user!.email! } })
+  const user = await prisma.user.findUnique({
+    where: { email: session!.user!.email! },
+  });
   if (!user) {
-    console.log('no user')
-    return Response.json({ error: 'Not authenticated', domains: [] }, { status: 401 })
+    console.log("no user");
+    return Response.json(
+      { error: "Not authenticated", domains: [] },
+      { status: 401 }
+    );
   }
 
-  let domains = await prisma.domain.findMany({ where: { userId: user.id } });
+  let domains = await prisma.domain.findMany({
+    where: { userId: user.id },
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   for (const d of domains) {
     // set domain verification key if it does not exist
     if (!d.domainVerificationKey) {
       const verificationKey = VerificationCodeGenerator.generate();
-      await prisma.domain.update({ where: { id: d.id }, data: { domainVerificationKey: verificationKey } });
+      await prisma.domain.update({
+        where: { id: d.id },
+        data: { domainVerificationKey: verificationKey },
+      });
     }
   }
 
   // refresh domains
   domains = await prisma.domain.findMany({ where: { userId: user.id } });
+  domains.sort((a, b) => {
+    // First compare by verification status
+    if (a.domainVerified !== b.domainVerified) {
+      // Put verified domains first
+      return b.domainVerified ? 1 : -1;
+    }
 
-  return Response.json({ domains, loaded: true }, { status: 200 })
+    // If verification status is the same, sort by name
+    return a.name.localeCompare(b.name);
+  });
+
+  return Response.json({ domains, loaded: true }, { status: 200 });
 }
 
 // post new domain
-export async function POST(
-  request: Request
-) {
+export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session || !session!.user) {
-    console.log('error: no session')
-    return Response.json({ error: 'Not authenticated', domains: [] }, { status: 401 })
+    console.log("error: no session");
+    return Response.json(
+      { error: "Not authenticated", domains: [] },
+      { status: 401 }
+    );
   }
 
-  const data = await request.json() as { name: string, domainName: string };
-  const { name, domainName } = data
+  const data = (await request.json()) as { name: string; domainName: string };
+  const { name, domainName } = data;
 
-  const user = await prisma.user.findUnique({ where: { email: session!.user!.email! } });
-  const existingDomain = await prisma.domain.findUnique({ where: { domainName } })
+  const user = await prisma.user.findUnique({
+    where: { email: session!.user!.email! },
+  });
+  const existingDomain = await prisma.domain.findUnique({
+    where: { domainName },
+  });
 
   if (existingDomain) {
-    return Response.json({ error: 'domain already exists' }, { status: 500 })
+    return Response.json({ error: "domain already exists" }, { status: 500 });
   }
 
   if (!user) {
-    return Response.json({ error: 'user not found' }, { status: 500 })
+    return Response.json({ error: "user not found" }, { status: 500 });
   }
 
   const domain = await prisma.domain.create({
@@ -78,9 +108,9 @@ export async function POST(
       name,
       domainName,
       userId: user.id,
-      crawlInterval: 24 * 60 // once a day
-    }
-  })
+      crawlInterval: 24 * 60, // once a day
+    },
+  });
 
-  return Response.json({ domain }, { status: 200 })
+  return Response.json({ domain }, { status: 200 });
 }
